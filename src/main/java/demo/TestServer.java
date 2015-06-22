177b098beab430c0027809917b3a53760497d437
jhernan <tpasensio@gmail.com>
2015-06-22 23:28:01 +0200

    initial tests

---
+++ src/main/java/demo/TestServer.java
@@ -0,0 +1,23 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@RestController
+//@ComponentScan
+public class TestServer {
+	@RequestMapping("/")
+	public String home(){
+		return "hola mundo";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TestServer.class, args);
+	}
+}
